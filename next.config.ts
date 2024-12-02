b29f6add44d093a0c629ca5e70df5fa9fc8ee58f
Einsy <eins.yeager@gmail.com>
2024-12-02 10:54:34 +0500

    portfolio move

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  output: "export"
+  output: "export",
+  images: {
+    unoptimized: true
+  }
   /* config options here */
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,145 @@
-import Image from "next/image";
-import Link from "next/link";
+"use client";
+/* eslint-disable @next/next/no-img-element */
+import { useState } from "react";
+import "./style.css";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Link href="/1">About</Link>
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [state, setState] = useState(1);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  return (
+    <>
+      <header className="header">
+        <div className="header_wrapper">
+          <div className="header_logo">
+            <a href="./">
+              {/* <img className="logo" src="assets/imgs/logo.png" alt="logo" /> */}
+            </a>
+          </div>
+          <nav className="header_nav">
+            <ul className="header_menu">
+              <li>
+                <a href="#">Contact</a>
+              </li>
+              <li>
+                <a href="./about.html">About</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <main className="main">
+        <div className="carousel_wrapper">
+          <div
+            className="carousel_control_left"
+            onClick={() => setState(state - 1 < 1 ? 1 : state - 1)}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+            <svg
+              className="arrow_left"
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+            >
+              <path d="M579-480 285-774q-15-15-14.5-35.5T286-845q15-15 35.5-15t35.5 15l307 308q12 12 18 27t6 30q0 15-6 30t-18 27L356-115q-15 15-35 14.5T286-116q-15-15-15-35.5t15-35.5l293-293Z" />
+            </svg>
+          </div>
+          <div
+            className="carousel"
+            style={{ transform: `translateX(-${state - 1}00%)` }}
+          >
+            <div className="carousel_item">
+              <img src="https://placehold.co/1400x800" alt="1" />
+            </div>
+            <div className="carousel_item">
+              <img src="https://placehold.co/1400x800/orange/white" alt="2" />
+            </div>
+            <div className="carousel_item">
+              <img src="https://placehold.co/1400x800/red/black" alt="3" />
+            </div>
+          </div>
+          <div
+            className="carousel_control_right"
+            onClick={() => setState(state + 1 > 3 ? 3 : state + 1)}
+          >
+            <svg
+              className="arrow_right"
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+            >
+              <path d="M579-480 285-774q-15-15-14.5-35.5T286-845q15-15 35.5-15t35.5 15l307 308q12 12 18 27t6 30q0 15-6 30t-18 27L356-115q-15 15-35 14.5T286-116q-15-15-15-35.5t15-35.5l293-293Z" />
+            </svg>
+          </div>
+        </div>
+        <div className="blog">
+          <div className="blog_item">
+            <img
+              className="blog_item_img"
+              src="https://placehold.co/600x600"
+              alt="1"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <div className="blog_item_content">
+              <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Eveniet consequuntur ducimus debitis beatae iusto nisi pariatur
+                nam ipsam, laborum eum tempora minus officiis et quibusdam
+                commodi similique autem illum veritatis!
+              </p>
+            </div>
+          </div>
+          <div className="blog_item">
+            <img
+              className="blog_item_img"
+              src="https://placehold.co/600x600"
+              alt="1"
+            />
+            <div className="blog_item_content">
+              <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+                quasi aliquam repellat harum soluta distinctio recusandae natus
+                accusantium fugit repellendus quidem numquam voluptas, hic ex
+                blanditiis, sapiente, adipisci doloremque enim?
+              </p>
+            </div>
+          </div>
+          <div className="blog_item">
+            <img
+              className="blog_item_img"
+              src="https://placehold.co/600x600"
+              alt="1"
+            />
+            <div className="blog_item_content">
+              <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+                perspiciatis, quibusdam nesciunt error unde iure at perferendis
+                necessitatibus, aliquam omnis eveniet soluta minus nostrum.
+                Eligendi autem rem praesentium corrupti explicabo?
+              </p>
+            </div>
+          </div>
+          <div className="blog_item">
+            <img
+              className="blog_item_img"
+              src="https://placehold.co/600x600"
+              alt="1"
+            />
+            <div className="blog_item_content">
+              <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+                perspiciatis, quibusdam nesciunt error unde iure at perferendis
+                necessitatibus, aliquam omnis eveniet soluta minus nostrum.
+                Eligendi autem rem praesentium corrupti explicabo?
+              </p>
+            </div>
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <footer className="footer">Footer</footer>
+    </>
   );
 }
